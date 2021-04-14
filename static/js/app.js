db8ba8474834dfb2c04a22a824b87a7041a8e893
Sam Ziegler <szig97@outlook.com>
2021-04-13 22:02:33 -0500

    adding filter code

--- static/js/app.js
+++ static/js/app.js
@@ -24,3 +24,34 @@ function deleteTbody() {
 // Initial display of UFO sightings
 console.log(tableData);
 tableDisplay(tableData);
+
+// 'Filter Table' button
+var button = d3.select("#filter-btn");
+
+// filter and display the database
+button.on("click", function(event) {
+    d3.event.preventDefault();
+    deleteTbody();
+    var dateInput = d3.select("#datetime").property("value");
+
+    if (dateInput.trim() === "") {
+        // display whole database if date field has no date
+        var filteredData = tableData;
+    }   else {
+        // otherwise, display filtered dataset
+        var filteredData = tableData.filter(ufoSighting =>
+            ufoSighting.datetime === dateInput.trim());
+    };
+
+    // if no records are found, display this message
+    if (filteredData.length == 0) {
+        d3.select("tbody")
+          .append("tr")
+          .append("td")
+            .attr("colspan", 7)
+            .html("<h4>No Records Found</h4>");
+    };
+
+    console.log(filteredData);
+    tableDisplay(filteredData);
+});
