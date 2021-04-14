d9553c3ec062143d0170738aae0d6322f3254743
Sam Ziegler <szig97@outlook.com>
2021-04-13 21:44:30 -0500

    adding function code and clearing table for new data

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,22 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// function to display UFO sightings
+function tableDisplay(ufoSightings) {
+    var tbody = d3.select("tbody");
+    ufoSightings.forEach((ufoRecord) => {
+        var row = tbody.append("tr");
+        Object.entries(ufoRecord).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.html(value);
+        });
+    });
+};
+
+
+// clear the table for new data
+function deleteTbody() {
+    d3.select("tbody")
+      .selectAll("tr").remove()
+      .selectAll("td").remove();
+}
